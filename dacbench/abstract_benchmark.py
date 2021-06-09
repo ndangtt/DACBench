e7b56377cdf609fddb314ed4d755aee94a2c2ae6
ndangtt <ndangt@gmail.com>
2021-06-09 19:43:36 +0100

    make bench .json config files pretty

--- dacbench/abstract_benchmark.py
+++ dacbench/abstract_benchmark.py
@@ -3,9 +3,47 @@ import numpy as np
 from gym import spaces
 from functools import partial
 from dacbench import wrappers
+import re
 
 # from dacbench import ModuleLogger
 
+class CompactJSONEncoder(json.JSONEncoder):
+    """A JSON Encoder that puts small lists on single lines."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.indentation_level = 0
+
+    def encode(self, o):
+        """Encode JSON object *o* with respect to single line lists."""
+
+        if isinstance(o, (list, tuple)):
+            if self._is_single_line_list(o):
+                return "[" + ", ".join(json.dumps(el) for el in o) + "]"
+            else:
+                self.indentation_level += 1
+                output = [self.indent_str + self.encode(el) for el in o]
+                self.indentation_level -= 1
+                return "[\n" + ",\n".join(output) + "\n" + self.indent_str + "]"
+
+        elif isinstance(o, dict):
+            self.indentation_level += 1
+            output = [self.indent_str + f"{json.dumps(k)}: {self.encode(v)}" for k, v in o.items()]
+            self.indentation_level -= 1
+            return "{\n" + ",\n".join(output) + "\n" + self.indent_str + "}"
+
+        else:
+            return json.dumps(o)
+
+    def _is_single_line_list(self, o):
+        if isinstance(o, (list, tuple)):
+            return not any(isinstance(el, (list, tuple, dict)) for el in o)\
+                   and len(o) <= 2\
+                   and len(str(o)) - 2 <= 60
+
+    @property
+    def indent_str(self) -> str:
+        return " " * self.indentation_level * self.indent
 
 class AbstractBenchmark:
     """
@@ -91,8 +129,11 @@ class AbstractBenchmark:
 
         conf["wrappers"] = self.jsonify_wrappers()
 
+        s = json.dumps(conf, indent=4, cls=CompactJSONEncoder)
         with open(path, "w") as fp:
-            json.dump(conf, fp)
+            #json.dump(conf, fp, cls=CompactJSONEncoder, indent=4)
+            fp.write(s)
+
 
     def jsonify_wrappers(self):
         wrappers = []
